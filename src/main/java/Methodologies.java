43d7da0e8c0fea327374e1300571dcfa32322abb
maggiewu12 <yellowdocious@gmail.com>
2022-10-03 08:21:35 -0500

    try5

--- src/main/java/Methodologies.java
+++ src/main/java/Methodologies.java
@@ -9,7 +9,7 @@ public class Methodologies {
 
     public static double howFast( int distanceInMiles, int hours, int minutes )
     {
-        return distanceInMiles/((minutes/60d)+hours);
+        return distanceInMiles/(hours + ((float)minutes/60f));
     }
 
     public static double getRootAdd( int a, int b, int c )
